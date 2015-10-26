bc70b869d6b652a088fdafaddbf3fd6e4ed13674
Amol Umrale <babaiscool@gmail.com>
2015-10-27 00:35:53 +0530

    Added new logic for extracting help from function doc string. Added class Singleton. Updated version for release.

--- mutils/misc/docstring.py
+++ mutils/misc/docstring.py
@@ -1,6 +1,7 @@
 import sys
 import inspect
 import re
+import os
 
 
 def trim(docstring):
@@ -31,28 +32,60 @@ def trim(docstring):
 	return '\n'.join(trimmed)
 
 
-def extract_help(func):
-	assert func.__doc__ is not None
-
-	argspec = inspect.getargspec(func)
-	if argspec.args[0] == 'self':
-		del argspec.args[0]
-
-	regex_args_help = ""
-	for arg in argspec.args:
-		regex_args_help += "((%s):(.*))"%arg
-
-	regex = re.compile("(.*)" + regex_args_help, re.M | re.S)
-
-	match = regex.match(func.__doc__)
-
+def extract_help_w_regex(func):
 	help = {}
-	if match:
-		help['help'] = match.group(1).strip()			#extract main help string
 
-		i = 3
-		for _ in range(len(argspec.args)):
-			help[match.group(i)] = match.group(i+1).strip()
-			i += 3
+	if func.__doc__ is not None:
+		argspec = inspect.getargspec(func)
+		if argspec.args[0] == 'self':
+			del argspec.args[0]
+
+		regex_args_help = "(.*)"				# short
+		for arg in argspec.args:
+			regex_args_help += "((%s):(.*))"%arg		# argument help
+		regex_args_help += "(?:\n\s*\n.*)"			# longer help text
+
+		regex = re.compile(regex_args_help, re.M | re.S)
+
+		match = regex.match(func.__doc__)
+
+		if match:
+			help['short'] = match.group(1).strip()				# extract short description
+
+			i = 3
+			for _ in range(len(argspec.args)):
+				help[match.group(i)] = match.group(i + 1).strip()
+				i += 3
+
+			help['long'] = match.group(i - 2).strip()			# extract long description
+
+		if getattr(func, '__extrahelp__', None) is not None:			# extract any extra help
+			help['extra'] = func.__extrahelp__.strip()
 
 	return help
+
+
+def extract_help(func):
+	help = {}
+
+	if func.__doc__ is not None:
+		argspec = inspect.getargspec(func)
+		item = 'short'
+
+		for line in func.__doc__.splitlines():
+			if len(line.strip()) == 0:
+				item = 'long'
+				continue
+
+			if line.find(':') > 0:
+				parts = line.split(':')
+				item = parts[0].strip()
+				help[item] = parts[1].strip()
+			else:
+				if item in help.keys():
+					help[item] += os.linesep + line.strip()
+				else:
+					help[item] = line.strip()
+
+	return help
+
