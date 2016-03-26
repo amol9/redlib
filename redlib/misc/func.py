fe8ec612aeb9d1a52ba5daffecafeec7ac4af941
Amol Umrale <babaiscool@gmail.com>
2016-03-27 03:56:43 +0530

    Add new caching mechanism with db for storing metadata
    Added class: Cache2
     it maintains a db for metadata
     each item can have a different timeout or none
     supports pickling data
    
    Added function str_time_period_to_seconds() for converting time period,
    like, 1d, 1h, etc. to seconds (from wallp/keep).
    
    Added function md5hash().
    
    Test: ok (via wallp).

--- redlib/misc/func.py
+++ redlib/misc/func.py
@@ -1,8 +1,11 @@
+import re
+from datetime import timedelta
+import hashlib
 
 from ..system.common import is_py2
 
 
-__all__ = ['ob']
+__all__ = ['ob', 'str_time_period_to_seconds', 'md5hash']
 
 
 def ob(input, key=b'48bldg03yghsl;741dfbml<?x8jgbv3805'):
@@ -23,4 +26,38 @@ def ob(input, key=b'48bldg03yghsl;741dfbml<?x8jgbv3805'):
 		return str(bytearray(output))
 	else:
 		return bytes(output)
+
+
+def str_time_period_to_seconds(period):
+
+	period_map = { 's': 'seconds', 'm': 'minutes', 'h': 'hours', 'd': 'days', 'w': 'weeks', 'M': 'months', 'Y': 'years' }
+	period_regex = re.compile("(\d{1,3})((s|m|h|d|w|M|Y))")
+
+	match = period_regex.match(period)
+	
+	if match is None:
+		raise TimeError('bad time period: %s'%period)
+
+	num = int(match.group(1))
+	if num <= 0 :
+		raise TimeError('bad time period: %s'%period)
+
+	abbr_period = match.group(2)
+	tdarg = {}	
+
+	if abbr_period == 'M':
+		tdarg['days'] = 30 * num
+	elif abbr_period == 'Y':
+		tdarg['days'] = 365 * num
+	else:
+		tdarg[period_map[abbr_period]] = num
+
+	td = timedelta(**tdarg)
+	return td.total_seconds()
+
+
+def md5hash(input):
+	md5h = hashlib.md5()
+	md5h.update(input)
+	return md5h.hexdigest()
 
