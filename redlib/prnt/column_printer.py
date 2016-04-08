79ba380813f3937557adf1763fe018cf0ef3ac17
Amol Umrale <babaiscool@gmail.com>
2016-04-08 21:19:49 +0530

    Add tests and fix issues for ColumnPrinter
    Tested: default, 2 column fixed width, wrap, 2 column wrap, fill, 2
    column auto fill, align, etc.

--- redlib/prnt/column_printer.py
+++ redlib/prnt/column_printer.py
@@ -4,10 +4,11 @@ from redlib.api.system import get_terminal_size
 from .func import prints, printn
 
 
-__all__ = ['ColumnPrinter']
+__all__ = ['ColumnPrinter', 'Column', 'ColumnPrinterError']
 
-# margin
+# margin, factor it in on width calc
 # CP inside CP
+# overlap
 
 class Callbacks:
 	def __init__(self):
@@ -17,14 +18,17 @@ class Callbacks:
 
 class Column:
 	
-	def __init__(self, width=1, fill=False, max=None, min=None, wrap=False, align='l', ratio=None, margin=None):
+	def __init__(self, width=1, fill=False, max=None, min=None, wrap=False, align='l', ratio=None, rmargin=1, lmargin=None, overlap=0):
 		self.width	= width
 		self.fill	= fill
 		self.max	= max
 		self.min	= min or 1
 		self.wrap	= wrap
 		self.align	= align
-		self.margin	= margin
+		self.ratio	= ratio
+		self.rmargin	= rmargin
+		self.lmargin	= lmargin
+		self.overlap	= overlap
 
 
 class ColumnPrinterError(Exception):
@@ -48,7 +52,7 @@ class ColumnPrinter:
 		tw, _ = get_terminal_size()
 		tw -= 2
 
-		if any(map(lambda c.width < 1, filter(lambda c : c.fill == False, self._cols))):
+		if any(map(lambda c : c.width < 1, filter(lambda c : c.fill == False, self._cols))):
 			raise ColumnPrinterError()
 
 		fill_cols = filter(lambda c : c.fill == True, self._cols)
@@ -61,7 +65,7 @@ class ColumnPrinter:
 		fill_width_avl = total_fill_width
 
 		for col in fill_cols:
-			if fill_width_avl < (col.ratio * total_fill_width):
+			if col.ratio is not None and fill_width_avl < (col.ratio * total_fill_width):
 				raise ColumnPrinterError()
 
 			if col.ratio is not None:
@@ -69,24 +73,21 @@ class ColumnPrinter:
 			else:
 				col.width = int(total_fill_width / len(fill_cols))
 
-			if and col.width < col.min:
+			if col.width < col.min:
 				raise ColumnPrinterError()
 			if col.max is not None and col.width > col.max:
 				col.width = col.max
 
-			fill_width_avl -= co.width
+			fill_width_avl -= col.width
 
 
 	def make_fmt_string(self):
 		fmt_string = u''
+		align_map = {'l': '<', 'r': '>', 'c': '^'}
 
 		i = 0
 		for c in self._cols:
-			align = '<'
-			if c.align == 'r':
-				align = '>'
-
-			fmt_string += u'{%d:%s%d} '%(i, align, c.width)
+			fmt_string += u'{%d:%s%d} '%(i, align_map[c.align], c.width)
 			i += 1
 
 		self._fmt_string = fmt_string
@@ -115,19 +116,21 @@ class ColumnPrinter:
 			col = self._cols[i]
 			if len(args_copy[i]) > col.width:
 				if col.wrap:		# wrap
-					wrapped = wrap(args_copy[i], self._last_col_width)
+					wrapped = wrap(args_copy[i], col.width)
 					args_copy[i] = wrapped if len(wrapped) > 0 else ['']
 					max_row = max(max_row, len(wrapped))
 				else:			# trim
-					args_copy[i] = [args_copy[i][0 : col_width]]
+					args_copy[i] = [args_copy[i][0 : col.width]]
 			else:
 				args_copy[i] = [args_copy[i]]
 
 
 		last_row = None
 		for i in range(0, max_row):
-			last_row = map(lambda a : a[i] if i < len(a) else '', args_copy))
-			prints(self._fmt_string.format(*last_row)
+			if i > 0:
+				print('')
+			last_row = map(lambda a : a[i] if i < len(a) else '', args_copy)
+			prints(self._fmt_string.format(*last_row))
 
 		if not self._row_not_cp:
 			print('')
--- redlib/test/prnt/test_column_printer.py
+++ redlib/test/prnt/test_column_printer.py
@@ -1,10 +1,68 @@
 from unittest import TestCase, main as ut_main
+from itertools import cycle
+
+from redlib.api.prnt import ColumnPrinter, Column, ColumnPrinterError
 
 
-TestColumnPrinter(TestCase):
+class TestColumnPrinter(TestCase):
 
-	def test_1(self):
+	def test_default(self):
+		cp = ColumnPrinter()
+		cp.printf('test123')
+		cp.printf('really long string ' * 10)
+
+
+	def test_2_col_fixed_width(self):
+		cp = ColumnPrinter(cols=[Column(width=20), Column(width=40)])
+		cp.printf('test', '123')
+		cp.printf('test2', 'really long string ' * 10)
+
+
+	def test_2_col_wrap_col_2(self):
+		cp = ColumnPrinter(cols=[Column(width=20), Column(width=40, wrap=True)])
+		cp.printf('test', '123')
+		cp.printf('test2', 'really long string ' * 10)
+
+
+	def test_2_col_wrap_both(self):
+		cp = ColumnPrinter(cols=[Column(width=20, wrap=True), Column(width=40, wrap=True)])
+		cp.printf('test', '123')
+		cp.printf('test2 ' * 5, 'really long string ' * 10)
+
+
+	def test_2_col_auto_fill(self):
+		cp = ColumnPrinter(cols=[Column(fill=True), Column(fill=True)])
+		s = self.gen_string(100)
+		cp.printf(s, s)
+
+
+	def test_3_col_fill_col_2(self):
+		cp = ColumnPrinter(cols=[Column(width=20), Column(fill=True, max=90), Column(width=10)])
+		s = self.gen_string(100)
+		cp.printf('test', s, '1234')
+
+
+	def test_3_col_fill_col_2_wrap(self):
+		cp = ColumnPrinter(cols=[Column(width=20), Column(fill=True, max=90, wrap=True), Column(width=10)])
+		s = self.gen_string(100)
+		cp.printf('test', s, '1234')
+
+
+	def test_align(self):
+		cp = ColumnPrinter(cols=[Column(width=20, align='l'), Column(width=20, align='r'), Column(width=20, align='c'), Column(width=20)])
+		cp.printf('left', 'right', 'center', 'left')
+
+
+	def test_ratio_2_col(self):
 		pass
+
+
+	def gen_string(self, n=10):
+		c = cycle('0123456789')
+		output = ''
+		for i in range(0, n):
+			output += c.next()
+		return output
 
 
 if __name__ == '__main__':
