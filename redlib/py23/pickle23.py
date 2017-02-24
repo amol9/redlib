d4aa3b76f544ca247fb1e3e4ee045a974971315e
amol9 <babaiscool@gmail.com>
2017-02-24 20:18:57 +0530

    Added functions, pickledumps() and pickleloads(), to pickle to and from string.

--- redlib/py23/pickle23.py
+++ redlib/py23/pickle23.py
@@ -3,7 +3,8 @@ import pickle
 from ..system.common import *
 
 
-__all__ = ['pickledump', 'pickleload']
+__all__ = ['pickledump', 'pickleload', 'pickledumps', 'pickleloads']
+
 
 
 def pickledump(obj, file, protocol=None, fix_imports=None):
@@ -19,3 +20,16 @@ def pickleload(file, **kwargs):
 	else:
 		return pickle.load(file)
 
+
+def pickledumps(obj, protocol=None, fix_imports=None):
+	if is_py3():
+		return pickle.dumps(obj, protocol=protocol, fix_imports=fix_imports)
+	else:
+		return pickle.dumps(obj, protocol=protocol)
+
+
+def pickleloads(s, **kwargs):
+	if is_py3():
+		return pickle.loads(s, **kwargs)
+	else:
+		return pickle.loads(s)
